b5c5bf729d97f4b6ad45883b23d523c5270f36a1
Suleiman010 <suleimananwarmubarak@gmail.com>
2026-04-08 12:30:20 +0300

    feat: build and style the layout of the sidebar component

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,28 @@
 import "./sidebar.css";
 
 export default function Sidebar() {
-	return <div>sidebar</div>;
+	return (
+		<div className="sidebar">
+			<div className="menu-icons">
+				<a href="/" className="menu-icon">
+					<img src="/general/logo.png" alt="Sidebar Logo" className="logo" />
+				</a>
+				<a href="/" className="menu-icon">
+					<img src="/general/home.svg" alt="Home icon" />
+				</a>
+				<a href="/" className="menu-icon">
+					<img src="/general/create.svg" alt="Create icon" />
+				</a>
+				<a href="/" className="menu-icon">
+					<img src="/general/updates.svg" alt="Update icon" />
+				</a>
+				<a href="/" className="menu-icon">
+					<img src="/general/messages.svg" alt="Messages icon" />
+				</a>
+			</div>
+			<a href="/" className="menu-icon">
+				<img src="/general/settings.svg" alt="Settings icon" />
+			</a>
+		</div>
+	);
 }
